f35b09c6fe0198d92be39e4556d030f975d07f29
Joe Betz <jpbetz@google.com>
2018-04-20 10:43:44 -0700

    clientv3: Fix auth client to use endpoints instead of host when dialing, fix tests to block on dial when required.

--- integration/cluster.go
+++ integration/cluster.go
@@ -520,6 +520,7 @@ type member struct {
 	PeerTLSInfo *transport.TLSInfo
 	// ClientTLSInfo enables client TLS when set
 	ClientTLSInfo *transport.TLSInfo
+	DialOptions   []grpc.DialOption
 
 	raftHandler   *testutil.PauseableHandler
 	s             *etcdserver.EtcdServer
@@ -709,6 +710,9 @@ func NewClientV3(m *member) (*clientv3.Client, error) {
 			return nil, err
 		}
 		cfg.TLS = tls
+	}
+	if m.DialOptions != nil {
+		cfg.DialOptions = append(cfg.DialOptions, m.DialOptions...)
 	}
 	return newClientV3(cfg)
 }
